07ee0f877ea1ffbfb97af8ecc7afb451c4a3bca9
Siwapol Techaratsami <ssiwapol@gmail.com>
2020-08-15 15:37:23 +0700

    computer mode

--- ConnectFour.java
+++ ConnectFour.java
@@ -3,12 +3,14 @@ import java.util.*;
 
 class ConnectFour{
 
+    // initial variable
     final int gridWidth = 7;
     final int gridHeight = 6;
     final int connect = 4;
     final String p1Char = "X";
     final String p2Char = "O";
-    private int[][] grid = new int[gridHeight][gridWidth];
+    private int[][] gridMain = new int[gridHeight][gridWidth];
+    private int[][] gridTest = new int[gridHeight][gridWidth];
     private int turn = 1;
     private int move = 0;
     private int mode = 0;
@@ -16,11 +18,11 @@ class ConnectFour{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException{
-
         ConnectFour c = new ConnectFour();
         c.start();
     }
 
+    // start program with welcome screen to choose game mode
     public void start() throws IOException{
 
         // welcome
@@ -37,13 +39,14 @@ class ConnectFour{
             System.out.print("Input [1-2]: ");
             mode = Integer.parseInt(br.readLine());
         }
-        // single-player mode
+        // single-player mode and choose computer level
         if (mode == 1){
             System.out.println("Please select computer level:");
             System.out.println("[1] Easy");
             System.out.println("[2] Medium");
-            while (comLv<1 || comLv>2){
-                System.out.print("Input [1-2]: ");
+            System.out.println("[3] Hard");
+            while (comLv<1 || comLv>3){
+                System.out.print("Input [1-3]: ");
                 comLv = Integer.parseInt(br.readLine());
             }
             modeSingle(comLv);
@@ -55,35 +58,7 @@ class ConnectFour{
 
     }
 
-    private void modeTwo() throws IOException{
-        int col;
-        while(true){
-            showGrid();
-            System.out.print("Player(" + pChar(turn) + ")'s turn [1-7]: ");
-            col = Integer.parseInt(br.readLine()) - 1;
-            if (turn==1){
-                if (isValid(col)){
-                    putDisc(col, turn);
-                    move += 1;
-                    if (isEnd()) {
-                        break;
-                    }
-                    turn += 1;
-                }
-            }
-            else {
-                if (isValid(col)){
-                    putDisc(col, turn);
-                    move += 1;
-                    if (isEnd()) {
-                        break;
-                    }
-                    turn -= 1;
-                }
-            }
-        }
-    }
-
+    // single-player mode
     private void modeSingle(int lv) throws IOException{
         int col;
         while(true){
@@ -91,8 +66,8 @@ class ConnectFour{
             if (turn==1){
                 System.out.print("Player(" + pChar(turn) + ")'s turn [1-7]: ");
                 col = Integer.parseInt(br.readLine()) - 1;
-                if (isValid(col)){
-                    putDisc(col, turn);
+                if (isValid(col, gridMain)){
+                    putDisc(col, turn, gridMain);
                     move += 1;
                     if (isEnd()) {
                         break;
@@ -104,8 +79,8 @@ class ConnectFour{
                 System.out.print("Computer(" + pChar(turn) + ")'s turn [1-7]: ");
                 col = calCol(lv);
                 System.out.println(col+1);
-                if (isValid(col)){
-                    putDisc(col, turn);
+                if (isValid(col, gridMain)){
+                    putDisc(col, turn, gridMain);
                     move += 1;
                     if (isEnd()) {
                         break;
@@ -116,23 +91,113 @@ class ConnectFour{
         }
     }
 
-    private int calCol(int lv){
-        ArrayList<Integer> colList = new ArrayList<Integer>();
-        int col = 0;
+    // two-player mode
+    private void modeTwo() throws IOException{
+        int col;
+        while(true){
+            showGrid();
+            System.out.print("Player(" + pChar(turn) + ")'s turn [1-7]: ");
+            col = Integer.parseInt(br.readLine()) - 1;
+            if (turn==1){
+                if (isValid(col, gridMain)){
+                    putDisc(col, turn, gridMain);
+                    move += 1;
+                    if (isEnd()) {
+                        break;
+                    }
+                    turn += 1;
+                }
+            }
+            else {
+                if (isValid(col, gridMain)){
+                    putDisc(col, turn, gridMain);
+                    move += 1;
+                    if (isEnd()) {
+                        break;
+                    }
+                    turn -= 1;
+                }
+            }
+        }
+    }
 
-        for (int i=0; i<gridWidth; i++) {
-            colList.add(i);
+    // calculate column for computer
+    private int calCol(int lv){
+        int col;
+
+        // check column that ables to put disc
+        ArrayList<Integer> colListValid = new ArrayList<Integer>();
+        for (int j=0; j<gridMain[0].length; j++) {
+            if (isValid(j, gridMain)){
+                colListValid.add(j);
+            }
         }
 
-        if (lv==1){
-            col = colList.get(new Random().nextInt(colList.size()));
+        // check column that can defeat competitor
+        ArrayList<Integer> colListToWin = new ArrayList<Integer>();
+        for (int j=0; j<colListValid.size(); j++){
+            gridCopy();
+            if (isValid(j, gridTest)){
+                putDisc(j, 2, gridTest);
+                if (isWin(2, gridTest)){
+                    colListToWin.add(j);
+                }
+            }
+        }
+
+        // check column that can be defeated
+        ArrayList<Integer> colListToDefend = new ArrayList<Integer>();
+        for (int j=0; j<colListValid.size(); j++){
+            gridCopy();
+            if (isValid(j, gridTest)){
+                putDisc(j, 1, gridTest);
+                if (isWin(1, gridTest)){
+                    colListToDefend.add(j);
+                }
+            }
+        }
+
+        // computer level2 check validate and winning column
+        if (lv==2){
+            if (colListToWin.size()>0){
+                col = colListToWin.get(new Random().nextInt(colListToWin.size()));
+            }
+            else{
+                col = colListValid.get(new Random().nextInt(colListValid.size()));
+            }
+        }
+        // computer level3 check validate, winning and defensive column
+        else if (lv==3){
+            if (colListToWin.size()>0){
+                col = colListToWin.get(new Random().nextInt(colListToWin.size()));
+            }
+            else if (colListToDefend.size()>0){
+                col = colListToDefend.get(new Random().nextInt(colListToDefend.size()));
+            }
+            else{
+                col = colListValid.get(new Random().nextInt(colListValid.size()));
+            }
+        }
+        // computer level1 check validate column
+        else{
+            col = colListValid.get(new Random().nextInt(colListValid.size()));
         }
 
         return col;
     }
 
+    // copy main grid to test grid
+    private void gridCopy(){
+        for(int i=0; i<gridMain.length; i++){
+            for(int j=0; j<gridMain[i].length; j++){
+                gridTest[i][j]=gridMain[i][j];
+            }
+        }
+    }
+
+    // check whether game ends
     private boolean isEnd(){
-        if (isWin(1) || isWin(2)){
+        if (isWin(1, gridMain) || isWin(2, gridMain)){
             showGrid();
             if (mode==1 && turn==2){
                 System.out.println("** COMPUTER(" + pChar(turn) + ") WIN **");
@@ -152,37 +217,40 @@ class ConnectFour{
         }
     }
 
-    private boolean isWin(int p){
+    // check whether player wins
+    private boolean isWin(int p, int[][] g){
 
+        // there are four direction to check
         int cntHorizontal;
         int cntVertical;
         int cntDiagonalUp;
         int cntDiagonalDown;
 
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid.length; j++){
+        // for loop inside grid
+        for(int i=0; i<g.length; i++){
+            for(int j=0; j<g.length; j++){
                 cntHorizontal = 0;
                 cntVertical = 0;
                 cntDiagonalUp = 0;
                 cntDiagonalDown = 0;
                 for(int k=0; k<connect; k++){
-                    if (j+k<grid[i].length){
-                        if (grid[i][j+k]==p){
+                    if (j+k<g[i].length){
+                        if (g[i][j+k]==p){
                             cntHorizontal+=1;
                         }
                     }
-                    if (i+k<grid.length){
-                        if (grid[i+k][j]==p){
+                    if (i+k<g.length){
+                        if (g[i+k][j]==p){
                             cntVertical+=1;
                         }
                     }
-                    if (i+k<grid.length && j+k<grid[i].length){
-                        if (grid[i+k][j+k]==p){
+                    if (i+k<g.length && j+k<g[i].length){
+                        if (g[i+k][j+k]==p){
                             cntDiagonalUp+=1;
                         }
                     }
-                    if (i-k>=0 && j+k<grid[i].length){
-                        if (grid[i-k][j+k]==p){
+                    if (i-k>=0 && j+k<g[i].length){
+                        if (g[i-k][j+k]==p){
                             cntDiagonalDown+=1;
                         }
                     }
@@ -200,6 +268,7 @@ class ConnectFour{
         return false;
     }
 
+    // check whether grid is full
     private boolean isFull(){
         if (move >= gridWidth * gridHeight){
             return true;
@@ -209,17 +278,18 @@ class ConnectFour{
         }
     }
 
-    private boolean isValid(int j){
-        if (j >= grid[0].length){
+    // check whether column is validated to put disc
+    private boolean isValid(int j, int[][] g){
+        if (j >= g[0].length){
             return false;
         }
         int n = 0;
-        for(int i=0; i<grid.length; i++){
-            if (grid[i][j]>0){
+        for(int i=0; i<g.length; i++){
+            if (g[i][j]>0){
                 n+=1;
             }
         }
-        if (n >= grid.length){
+        if (n >= g.length){
             return false;
         }
         else{
@@ -227,34 +297,37 @@ class ConnectFour{
         }
     }
 
-    private void putDisc(int j, int p){
+    // put disc to grid
+    private void putDisc(int j, int p, int[][] g){
         int n = 0;
-        for(int i=0; i<grid.length; i++){
-            if (grid[i][j]>0){
+        for(int i=0; i<g.length; i++){
+            if (g[i][j]>0){
                 n+=1;
             }
         }
-        grid[n][j] = p;
+        g[n][j] = p;
     }
 
+    // show main grid in console
     private void showGrid(){
-        //resetConsole();
+        resetConsole();
 
         // show grid (reverse i)
-        for(int i=grid.length-1; i>=0; i--){
-            for(int j=0; j<grid[i].length; j++){
-                System.out.print("[" + pChar(grid[i][j]) + "]");
+        for(int i=gridMain.length-1; i>=0; i--){
+            for(int j=0; j<gridMain[i].length; j++){
+                System.out.print("[" + pChar(gridMain[i][j]) + "]");
             }
             System.out.println();
         }
 
         // show column number
-        for(int i=1; i<=grid[0].length; i++){
+        for(int i=1; i<=gridMain[0].length; i++){
             System.out.print(" " + i + " ");
         }
         System.out.println();
     }
 
+    // print plater character
     private String pChar(int p){
         if (p==1){
             return p1Char;
@@ -267,6 +340,7 @@ class ConnectFour{
         }
     }
 
+    // reset console
     private void resetConsole(){
         System.out.print("\033[H\033[2J");
         System.out.flush();
